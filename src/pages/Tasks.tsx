1ff9aa47a8690ae5f01d4bb36efb26259bdbad5c
freelancersifat627e <freelancersifat627@gmail.comm>
2025-01-09 00:53:20 +0600

    filter work done

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,15 +1,45 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import TaskCard from "@/module/TaskCard";
 import AddTaskModal from "@/redux/features/task/AddTaskModal";
-import { selectTasks } from "@/redux/features/task/taskSlice";
-import { useAppSelector } from "@/redux/hook";
+import { selectTasks, updateFilter } from "@/redux/features/task/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 const Tasks = () => {
   const tasks = useAppSelector(selectTasks);
   console.log(tasks);
+  const dispatch = useAppDispatch();
   return (
     <div className="container mx-auto px-5 py-10 mt-10">
-      <div className="flex justify-between items-center">
-        <h1>All Tasks is Here</h1>
+      <div className="flex justify-end gap-5 items-center">
+        <h1 className="flex-1">All Tasks is Here</h1>
+        <Tabs defaultValue="all">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("all"))}
+              value="all"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("low"))}
+              value="low"
+            >
+              Low
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("medium"))}
+              value="medium"
+            >
+              Medium
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("high"))}
+              value="high"
+            >
+              High
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
         <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -55,17 +55,33 @@ const taskSlice = createSlice({
     deleteTask: (state, action: PayloadAction<string>) => {
       state.tasks = state.tasks.filter((task) => task.id !== action.payload);
     },
+    updateFilter: (
+      state,
+      action: PayloadAction<"all" | "low" | "medium" | "high">
+    ) => {
+      state.filter = action.payload;
+    },
   },
 });
 
 export const selectTasks = (state: RootState) => {
-  return state.tasks.tasks;
+  const filter = state.tasks.filter;
+  if (filter === "low") {
+    return state.tasks.tasks.filter((task) => task.priority === "low");
+  } else if (filter === "medium") {
+    return state.tasks.tasks.filter((task) => task.priority === "medium");
+  } else if (filter === "high") {
+    return state.tasks.tasks.filter((task) => task.priority === "high");
+  } else {
+    return state.tasks.tasks;
+  }
 };
 
 export const selectFilter = (state: RootState) => {
   return state.tasks.filter;
 };
 
-export const { addTask, toggleCompletedState, deleteTask } = taskSlice.actions;
+export const { addTask, toggleCompletedState, deleteTask, updateFilter } =
+  taskSlice.actions;
 
 export default taskSlice.reducer;
